1ca1d0fa6ab22d1d051e1da3fc4cd485940665e6
Thomas Harris <thomas@webkite.com>
2017-10-03 20:51:32 -0400

    allow for building pdf from interwebs

--- build-resume-pdf.sh
+++ build-resume-pdf.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-wkhtmltopdf http://localhost:4000/resume tk-resume.pdf
-curl http://localhost:4000/resume/ | html-to-text > tk-resume.txt
+if [ $# -eq 0 ]
+  then
+    TARGET=http://localhost:4000
+  else
+    TARGET=$1
+fi
+
+wkhtmltopdf $TARGET tk-resume.pdf
+curl $TARGET | html-to-text > tk-resume.txt
